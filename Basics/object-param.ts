74917f3211bc09320bb6e8be748928d7342f9ca6
SkylarSiggard <siggardskylar@gmail.com>
2023-03-25 15:01:14 -0600

    better notes

--- Basics/object-param.ts
+++ Basics/object-param.ts
@@ -4,7 +4,8 @@ interface Add {
     first: number;
     second: number;
 }
-//todo We can set the param to our interface typing. 
+//todo we can do three different ways to pass this as a function. Inline, type or interface
+//! Creating a Type or interface is the better options and doesnt really matter which one you do. 
 export const addTwoNumbers = (params: Add) => {
     return params.first + params.second;
 };
